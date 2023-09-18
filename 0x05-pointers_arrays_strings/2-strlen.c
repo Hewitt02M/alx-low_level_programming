c79c766e4585511c7c69e652121b269c5e101d1a
Hewitt02M <masakurehewitt21@gmail.com>
2023-09-18 08:19:33 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *   * _strlen - return the lenth of the string as an int value
+ *     * @s: the string to measure.
+ *       *
+ *         * Return: Int value of the lenth.
+ */
+int _strlen(char *s)
+{
+	int t = 0;
+
+	while (*(s + t) != '\0')
+	{
+		t++;
+	}
+	return (t);
+}
