045087c2ddb1c7984c7b017ebcb4dfd44dbc3403
Hewitt02M <masakurehewitt21@gmail.com>
2023-10-04 06:17:26 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,9 +9,10 @@
  */
 int main(int argc, char *argv[])
 {
-int n;
-for (n = 0; n < argc; n++)
-printf("argv[%2d]: %s\n", n, argv[n]);
+int nx;
+
+for (nx = 0; nx < argc; nx++)
+printf("%s\n", argv[nx]);
 
 return (0);
 }
