558775025c12bf2af42269b191b5aae09b6cf2c1
Hewitt02M <masakurehewitt21@gmail.com>
2024-02-21 15:54:34 -0800

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,41 +1,22 @@
 #!/usr/bin/python3
-""" This module computes the perimeter of an island"""
+"""5-island_perimeter module"""
 
 
 def island_perimeter(grid):
-    """ island_perimeter:
-                    Args: grid, a list of list of integers
-                    Return: perimeter of the island described in grid
-    """
+    """Returns the perimeter of the island described in grid"""
+    c = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
 
-    perimeter = 0
-    env_list = []
-
-    for items in range(len(grid)):
-        if items == 0 or items == len(grid):
-            continue
-        for cell in range(len(grid[items])):
-            if cell == 0 or cell == len(grid[items]):
-                continue
-            else:
-                if grid[items][cell] == 1:
-                    env_list = [grid[items - 1][cell], grid[items][cell + 1],
-                                grid[items + 1][cell],  grid[items][cell - 1]]
-                    count = 0
-                    for j in env_list:
-                        if j == 0:
-                            count = count + 1
-                    if (count == 4):
-                        return 4
-                    elif (count == 3):
-                        perimeter = perimeter + 3
-                    elif (count == 2):
-                        perimeter = perimeter + 2
-                    elif (count == 1):
-                        perimeter = perimeter + 1
-                    else:
-                        pass
-                    env_list = []
-                else:
-                    pass
-    return perimeter
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == width or grid[i][j + 1] != 1:
+                    c += 1
+                if i == length or grid[i + 1][j] != 1:
+                    c += 1
+    return c
